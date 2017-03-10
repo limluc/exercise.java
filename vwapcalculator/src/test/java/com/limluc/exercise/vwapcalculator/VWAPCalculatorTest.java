2160fdc701cd6fbed5e4aacae2006fa4e49b014f
Lucky Lim <lucky.a.lim@gmail.com>
2017-03-10 01:23:39 +0000

    #0 Initialize VWapCalculator Structure with Test

---
+++ vwapcalculator/src/test/java/com/limluc/exercise/vwapcalculator/VWAPCalculatorTest.java
@@ -0,0 +1,22 @@
+package com.limluc.exercise.vwapcalculator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VWAPCalculatorTest {
+
+    //undertest
+    private Calculator calculator;
+
+    @Before
+    public void setUp() throws Exception {
+        calculator = new VWAPCalculator();
+    }
+
+    @Test
+    public void shouldCalculateVWAP() throws Exception {
+        fail("Not Implemented Yet");
+    }
+}
